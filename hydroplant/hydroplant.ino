ec5bb62ddec48013a3611af3d4d5d7ba596f0ca4
Marc Widmer <marc@dymax.io>
2015-08-14 16:56:08 +0200

    fixed pumping & notification bugs. better led notification.

--- hydroplant/hydroplant.ino
+++ hydroplant/hydroplant.ino
@@ -1,4 +1,4 @@
-//DYMXIØN HYDROPLANT V.0.3.0
+//DYMXIØN HYDROPLANT V.0.3.1
 //using cactus micro arduino compatible with wifi esp2866 by april brothers
 //http://wiki.aprbrother.com/wiki/Cactus_Micro
 
@@ -22,7 +22,7 @@ Sleep sleep;
 //time until next plant check
 //6 hours = 21 600 000 milliseconds -> checks plant watering 4 times a day
 //TODO change for testing and set properly before sealing watering system 
-unsigned long sleepTime = 21600000; //set sleep time in ms, max sleep time is 49.7 days
+unsigned long sleepTime = 20000; //set sleep time in ms, max sleep time is 49.7 days
 boolean setSleep = true;
 boolean sentNotification = false;
 
@@ -32,8 +32,8 @@ boolean sentNotification = false;
 //WIFI SETTINGS, 2.4 GHZ B-Mode!
 //If conection is not working, check that your wireless router is configured to support B-mode
 //Usually B/G/N-Mixed which should support all connection types (20Mhz seems to be preferred for B-Mode)
-#define SSID "dymaxion-guest"
-#define PASS "buckm1nst3r!"
+#define SSID "hydroplant"
+#define PASS "hydroplant"
 
 //pushingbox device id, used to send email or mobile notifications (if water level is low -> refill needed)
 #define DEVID "v06CCA7D5005850A"
@@ -41,7 +41,7 @@ boolean sentNotification = false;
 //like e-mail, twitter, android and iphone notification
 
 //DEBUG SETTINGS
-#define DEBUG 1 //uncomment to enable debug
+//#define DEBUG 1 //uncomment to enable debug
 
 #ifdef DEBUG
 #define DEBUG_PRINT(x)     Serial.print(x)
@@ -60,7 +60,7 @@ int waterPin = A6; //water level sensor analog input
 
 
 //potentiometer controls
-int soilPotPin = A10; //set the humidity limit with a potentiometer
+int soilPotPin = A1; //set the humidity limit with a potentiometer
 int waterPotPin = A2; //set the duration of pump (needs some mapping)
 
 //sensor power pins
@@ -72,29 +72,30 @@ int soilSensorPowerPin2 = 9;
 
 int soilSense = 0; //stores soil moisture sensed value (0-1024)
 int waterSense = 0; //stores water level sensed value (0-1024)
-int waterLowLimit = 20; //stop pump -> send alert, make slightly higher then 0 to avoid jitter 
+int waterLowLimit = 100; //stop pump -> send alert, make slightly higher then 0 to avoid jitter 
 int soilMoistureLimit = 150; //change this according to your plant (0-100 is really dry, 350-500 is rather normal)
 int waterPumpingLimit = 200;
 
 unsigned long waterPumpDuration = 60000; //in ms, controlled via potentiometer (5000-6000 ms)
-unsigned long notificationDuration = 14400000; //in ms -> adjust the duration in which interval you would like to receive a notification if plant needs water 
+unsigned long notificationDuration = 3600000; //in ms -> adjust the duration in which interval you would like to receive a notification if plant needs water 
 
 //led if wifi is ready
-int ledWifiPin = 18;
+int ledPlantPin = 18;
 
 //water warning light
-int ledWaterPin = 6;
+int ledWaterPin = 10;
 
 //water pump pin
 int pumpPin = 21;
 boolean startPumping = false; //used to determine if water pump is currently in use
 
 //inital settings
-int ledState = LOW;             // ledState used to set the LED
-long previousMillis = 0;        // will store last time LED was updated
+int ledStatePlant = LOW; 
+int ledStateWater = LOW;              // ledState used to set the LED
+long previousMillisPlant = 0;        // will store last time LED was updated
+long previousMillisWater = 0; 
 
-//blink timer if wifi connected
-long interval = 1000;           // interval at which to blink (milliseconds)
+
 
 //software serial to communicate with esp2866 wifi
 char serialbuffer[100];//serial buffer for request command
@@ -116,9 +117,9 @@ void setup()
     digitalWrite(pumpPin, LOW); //disable pin 
     
     //leds setup
-    pinMode(ledWifiPin, OUTPUT);
+    pinMode(ledPlantPin, OUTPUT);
     pinMode(ledWaterPin, OUTPUT);
-    digitalWrite(ledWaterPin, LOW);
+    //digitalWrite(ledWaterPin, HIGH);
 
     //power sensor pin setup
     pinMode(waterSensorPowerPin1, OUTPUT);
@@ -179,38 +180,6 @@ void loop()
 
 
 
-  //remove later if it still works without
-  
-  //output everything from ESP8266 to the Arduino Micro Serial output
-  //TODO: check disable after working implementation
-  /*
-  while (mySerial.available() > 0) {
-    Serial.write(mySerial.read());
-  }
-
-  //to send commands to the ESP8266 from serial monitor (ex. check IP addr)
-  if (Serial.available() > 0) {
-     //read from serial monitor until terminating character
-     int len = Serial.readBytesUntil('\n', serialbuffer, sizeof(serialbuffer));
-
-     //trim buffer to length of the actual message
-     String message = String(serialbuffer).substring(0,len-1);
-     
-     DEBUG_PRINT("message: ");
-     DEBUG_PRINT(message);
-     DEBUG_PRINTLN("");
-     //Serial.println("message: " + message);
-
-     //check to see if the incoming serial message is an AT command
-     if(message.substring(0,2)=="AT"){
-       //make command request
-       DEBUG_PRINTLN("COMMAND REQUEST");
-       mySerial.println(message); 
-     }//if not AT command, ignore
-  }
-  */  
-
-
   //read soil & water sensors values
   soilSense = analogRead(soilPin);
   waterSense = analogRead(waterPin);
@@ -221,6 +190,7 @@ void loop()
   
   
   //output to console
+  
   DEBUG_PRINTLN("CUSTOM SENSOR VALUES------------------");
   DEBUG_PRINT("Soil humidity threshold: ");
   DEBUG_PRINTDEC(soilMoistureLimit);
@@ -235,11 +205,15 @@ void loop()
   DEBUG_PRINTDEC(waterSense);
   DEBUG_PRINTLN("");
 
-  //check if the plant needs water
-  if (soilSense <= soilMoistureLimit) {
-    //if there still is enough water, pump it
-    digitalWrite(ledWifiPin, HIGH);
-    if (waterSense > waterLowLimit) {
+  #ifdef DEBUG
+    delay(1000); 
+  #endif  
+
+  if (waterSense > waterLowLimit) {
+    //has water available
+    
+    if (soilSense <= soilMoistureLimit) {
+      //give water
 
       //water is available -> reset notification
       sentNotification = false; 
@@ -250,23 +224,31 @@ void loop()
       }
 
       if (pumpTimeElapsed > waterPumpDuration) {
-        startPumping = false; //reset pump
-        digitalWrite(pumpPin, LOW);  //disable pump 
         DEBUG_PRINTLN("pumping stopped");
         gotoSleep(); //goto sleep after pumping is finished
       }
       else {
+        digitalWrite(ledPlantPin, LOW);
+        blinkWaterLed(50);
         digitalWrite(pumpPin, HIGH); //pump some water
-        DEBUG_PRINTLN("currently pumping");
-      }   
+        //DEBUG_PRINTLN("currently pumping");
+      }  
+
+
+
+    }
+    else {
+        //no water needed
+        gotoSleep();
     }
 
-    // otherwise send alert notification
-    else {
-
+  }
+  else {
+    //no water!
       digitalWrite(pumpPin, LOW); //disable pumping
       startPumping = false;
 
+      
       if (sentNotification == false) {
         notificationTimeElapsed = 0; //reset counter once for this notification cycle
         sentNotification = true; // cycle can only be triggered once
@@ -276,27 +258,20 @@ void loop()
       if (notificationTimeElapsed > notificationDuration) {
         sentNotification = false; //reset notification
       }
+      
   
       //blink water notification light
-      blinkWaterLed();
-    }
+      digitalWrite(ledPlantPin, LOW);
+      blinkWaterLed(250);
   }
-  else {
-    //plant needs no water, so just go to sleep
-    gotoSleep();
-  }
+  
 
-  //slow down output to read serial monitor in debug mode
-  #ifdef DEBUG
-    delay(1000); 
-  #endif
- 
 }
 
 void gotoSleep() {
   //disable leds
   digitalWrite(ledWaterPin, LOW);
-  digitalWrite(ledWifiPin, LOW);
+  digitalWrite(ledPlantPin, HIGH);
 
   //reset pump
   startPumping = false; //reset pump
@@ -305,7 +280,11 @@ void gotoSleep() {
   
 
   #ifdef DEBUG
-    delay(20000); //delay to simulate sleep
+    //sleep until next wake-up 
+    DEBUG_PRINT("sleeping for: ");
+    DEBUG_PRINTDEC(sleepTime);
+    DEBUG_PRINTLN("");
+    delay(sleepTime); //simulate sleep
   #else
     //sleep until next wake-up 
     DEBUG_PRINT("sleeping for: ");
@@ -318,22 +297,42 @@ void gotoSleep() {
   
 }
 
-void blinkWaterLed() {
+void blinkWaterLed(int interval) {
   //enable water notification, blink water
   unsigned long currentMillis = millis();
-  if(currentMillis - previousMillis > interval) {
+  if(currentMillis - previousMillisWater > interval) {
     // save the last time you blinked the LED 
-    previousMillis = currentMillis;   
+    previousMillisWater = currentMillis;   
 
     // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
+    if (ledStateWater == LOW) {
+      ledStateWater = HIGH;
     }
     else {
-      ledState = LOW;
+      ledStateWater = LOW;
     }
     // set the LED with the ledState of the variable:
-    digitalWrite(ledWaterPin, ledState);
+    digitalWrite(ledWaterPin, ledStateWater);
+  }
+}
+
+
+void blinkPlantLed(int interval) {
+  //enable water notification, blink water
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillisPlant > interval) {
+    // save the last time you blinked the LED 
+    previousMillisPlant = currentMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledStatePlant == LOW) {
+      ledStatePlant = HIGH;
+    }
+    else {
+      ledStatePlant = LOW;
+    }
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPlantPin, ledStatePlant);
   }
 }
 
@@ -356,12 +355,12 @@ void sendWaterAlert() {
     connectWifi();
     delay(1000);
     //turn off wifi notification led
-    //digitalWrite(ledWifiPin , LOW);
+    //digitalWrite(ledPlantPin , LOW);
   }
 
   //if wifi is connected -> light notification led
   else {
-    //digitalWrite(ledWifiPin , HIGH);
+    //digitalWrite(ledPlantPin , HIGH);
 
       //create start command
   String startcommand = "AT+CIPSTART=\"TCP\",\"koga.cx\", 80";
@@ -409,7 +408,7 @@ void sendWaterAlert() {
    delay(1000);
    mySerial.println("AT+CIPCLOSE");
    delay(5000);
-   //digitalWrite(ledWifiPin , LOW); //turn off wifi led after sending message
+   //digitalWrite(ledPlantPin , LOW); //turn off wifi led after sending message
     
   }
 
